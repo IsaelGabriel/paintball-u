c85d616d384845bb343410aa9f410b00e2f19823
Isael Gabriel <isaelgsfaria@gmail.com>
2025-08-24 04:35:00 -0300

    Add slide

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,8 +4,14 @@ class_name Player
 
 const GRAVITY = 15
 const SPEED = 10
+const SLIDE_SPEED = 40
 const JUMP_FORCE = 15
 const STOMP_FORCE = 40
+const JUMP_STEER_FACTOR = 0.5
+
+var sliding:bool = false
+var slide_direction:Vector2 = Vector2.ZERO
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -17,12 +23,30 @@ func _process(delta: float) -> void:
 	_move(delta)
 
 func _move(delta: float) -> void:
-	var movement_input = Input.get_vector("p_left", "p_right", "p_back", "p_forward").normalized()
-	velocity.x = movement_input.x * SPEED
-	velocity.z = -movement_input.y * SPEED
+	if not Input.is_action_pressed("p_slide"):
+		sliding = false
 	
-	if Input.is_action_just_pressed("p_jump") and is_on_floor():
-		velocity.y = JUMP_FORCE
-
-	if Input.is_action_just_pressed("p_slide") and not is_on_floor():
-		velocity.y = -STOMP_FORCE
+	
+	var movement_input = Input.get_vector("p_left", "p_right", "p_back", "p_forward").normalized()
+	
+	if is_on_floor():
+		
+		
+		if Input.is_action_just_pressed("p_jump"):
+			velocity.y = JUMP_FORCE
+		elif Input.is_action_pressed("p_slide") && not movement_input.is_zero_approx():
+			if not sliding:
+				slide_direction = movement_input
+			sliding = true
+			
+		
+		if not sliding:
+			velocity.x = movement_input.x * SPEED
+			velocity.z = -movement_input.y * SPEED
+	else:
+		if Input.is_action_just_pressed("p_slide"):
+			velocity.y = -STOMP_FORCE
+		
+	if sliding:
+		velocity.z = -slide_direction.y * SLIDE_SPEED
+		velocity.x = slide_direction.x * SLIDE_SPEED
