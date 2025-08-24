05ecf779ac641801c3fc5bef91b87a7fa42600ea
Isael Gabriel <isaelgsfaria@gmail.com>
2025-08-24 04:13:19 -0300

    Add player jump and skybox

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody3D
 class_name Player
 
 const GRAVITY = 9.8
-
-const SPEED = 5
+const SPEED = 10
+const JUMP_FORCE = 15
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -16,6 +16,9 @@ func _process(delta: float) -> void:
 	_move(delta)
 
 func _move(delta: float) -> void:
-	var movement_input = Input.get_vector("p_left", "p_right", "p_back", "p_forward")
+	var movement_input = Input.get_vector("p_left", "p_right", "p_back", "p_forward").normalized()
 	velocity.x = movement_input.x * SPEED
 	velocity.z = -movement_input.y * SPEED
+	
+	if Input.is_action_just_pressed("p_jump") and is_on_floor():
+		velocity.y = JUMP_FORCE
