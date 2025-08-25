54eedc15c91915a5e916a994a9ebe5e10b7b4f50
IsaelGSF <isaelgsfaria@gmail.com>
2025-08-25 00:06:14 -0300

    Jump buffer added

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -11,6 +11,11 @@ const STOMP_FORCE = 40
 const JUMP_STEER_FACTOR = 0.5
 const SLIDE_STEER = 20
 
+# Buffer
+const JUMP_BUFFER = 0.15
+
+var jump_buffer_count = 0.0
+
 var sliding:bool = false
 var slide_direction:Vector2 = Vector2.ZERO
 
@@ -31,17 +36,20 @@ func _move(delta: float) -> void:
 	
 	var movement_input = Input.get_vector("p_left", "p_right", "p_forward", "p_back").normalized().rotated(-rotation.y)
 	
+	if jump_buffer_count > 0.0:
+		jump_buffer_count -= delta
+	
 	if is_on_floor():
 		
 		if not sliding:
 			velocity.x = movement_input.x * SPEED
 			velocity.z = movement_input.y * SPEED
-		if Input.is_action_just_pressed("p_jump"):
+		if jump_buffer_count > 0.0:
 			velocity.y = JUMP_FORCE
 			sliding = false
 			velocity.x += movement_input.x * MOVEMENT_JUMP_FORCE
 			velocity.z += movement_input.y * MOVEMENT_JUMP_FORCE
-		elif Input.is_action_pressed("p_slide") && not movement_input.is_zero_approx():
+		elif Input.is_action_pressed("p_slide") and not movement_input.is_zero_approx():
 			if not sliding:
 				slide_direction = movement_input
 			sliding = true
@@ -63,4 +71,7 @@ func _move(delta: float) -> void:
 			velocity.z += velocity_difference.y * SLIDE_STEER * delta
 			velocity.x += velocity_difference.x * SLIDE_STEER * delta
 			
+	if Input.is_action_just_pressed("p_jump"):
+		jump_buffer_count = JUMP_BUFFER
+	
 			
