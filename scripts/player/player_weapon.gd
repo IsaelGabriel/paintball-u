90e85bda4ad5712dfdadc3ec591be7597704ca2f
IsaelGSF <isaelgsfaria@gmail.com>
2025-08-26 23:45:55 -0300

    Add basic splatter

--- scripts/player/player_weapon.gd
+++ scripts/player/player_weapon.gd
@@ -5,11 +5,11 @@ class_name PlayerWeapon
 const RAY_LENGTH = 1000
 
 var damage = 2
-var cooldown = .9
+var cooldown = .3
 var cooldown_count = 0.0
 
 @export var ray_bullet: PackedScene
-
+@export var splatter: PackedScene
 
 func _process(delta: float) -> void:
 	if cooldown_count > 0:
@@ -30,7 +30,7 @@ func _physics_process(delta: float) -> void:
 		#raycast_obj.position = global_position
 		#raycast_obj.target_position = end
 		#get_tree().current_scene.add_child(raycast_obj)
-		
+		var new_splatter: Node3D = null
 		var hit: Dictionary = space_state.intersect_ray(query)
 		if not hit.is_empty():
 			end = hit["position"]
@@ -38,11 +38,19 @@ func _physics_process(delta: float) -> void:
 				for child in hit["collider"].get_children():
 					if child is Damageable:
 						child.deal_damage(get_parent().get_parent(), damage)
+			# Create splatter
+			new_splatter = splatter.instantiate()
+			new_splatter.visible = false
+			new_splatter.look_at(hit["normal"])
+			print(hit["normal"])
+			hit["collider"].add_child(new_splatter)
+			new_splatter.global_position = hit["position"]
 		
-		var bullet = ray_bullet.instantiate()
+		var bullet: RayBulletTrail = ray_bullet.instantiate()
 		bullet.position = global_position
 		bullet.start = global_position
 		bullet.direction = (end - global_position).normalized()
 		bullet.end = end
+		bullet.splatter = new_splatter
 		get_tree().current_scene.add_child(bullet)
 		
--- scripts/ray_bullet_trail.gd
+++ scripts/ray_bullet_trail.gd
@@ -3,14 +3,18 @@ extends Node3D
 class_name RayBulletTrail
 
 var end: Vector3
-var speed: float = 75
+var speed: float = 125
 var start: Vector3
 var direction: Vector3
 var t: float
+var splatter: Node3D = null
 
 func _process(delta: float) -> void:
 	t += delta/(end - start).length() * speed
 	position = start.lerp(end, t)
 	
 	if t >= 1:
+		if splatter != null:
+			splatter.visible = true
+		
 		queue_free()
