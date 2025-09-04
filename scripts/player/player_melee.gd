2e38c2f029291a1f50e5be110a8b03d3da3d88e6
IsaelGSF <isaelgsfaria@gmail.com>
2025-09-04 04:23:57 -0300

    Add damage to hammer

--- scripts/player/player_melee.gd
+++ scripts/player/player_melee.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
+@export var damage: float = 2
+
 @onready var animator: AnimationPlayer = $AnimationPlayer
+@onready var player: Player = get_parent().get_parent()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("p_melee") and not visible:
@@ -10,3 +13,10 @@ func _process(delta: float) -> void:
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	visible = false
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Damageable") and visible:
+		for child in body.get_children():
+			if child is not Damageable: continue
+			child.deal_damage(player, damage)
